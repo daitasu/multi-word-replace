9a0c32aadd4d883cf6d980f0ca3f13d6e6721386
daitasu <ifctp.dai.monara@gmail.com>
2022-10-16 19:58:04 +0900

    chore: Add quasar default settings

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+import '@quasar/extras/material-icons/material-icons.css' // icon libraries
+import 'quasar/src/css/index.sass' // Quasar css
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const myApp = createApp(App)
+
+myApp.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+})
+
+myApp.mount('#app')
